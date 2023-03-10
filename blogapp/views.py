0219fdaf9d93d5e8628e300c4b11149ef8c9ed66
mortalBro <abhijeetk@triazinesoft.com>
2023-03-10 16:27:08 +0530

    first commit

---
+++ blogapp/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .forms import SignUpForm,LoginForm,PostForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from .models import Post
+from django.contrib.auth.models import Group
+
+def home(request):
+    posts=Post.objects.all() 
+    return render(request, 'blogapp/home.html',{'posts':posts})#
+
+def about(request):
+    return render(request ,'blogapp/about.html')
+
+def contact(request):
+    return render(request ,'blogapp/contact.html')
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        user = request.user
+        full_name = user.get_full_name()
+        gps=user.groups.all()
+        return render(request ,'blogapp/dashboard.html',{'posts':posts,'full_name':full_name,'groups':gps})
+    else:
+        return HttpResponseRedirect('/login.html/')
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def user_signup(request):
+    if request.method=="POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,"Congraluations ! You have become an Author, Thanks for be member!")
+            user = form.save()
+            group = Group.objects.get(name='Author')
+            user.groups.add(group)
+    else:
+        form = SignUpForm()
+    return render(request ,'blogapp/signup.html',{'form':form})
+
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method=="POST":
+            form = LoginForm(request=request ,data=request.POST)
+            if form.is_valid():
+                uname=form.cleaned_data['username']
+                upassword=form.cleaned_data['password']
+                user=authenticate(username=uname,password=upassword)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,"Loged in success")
+                    return HttpResponseRedirect('/dashboard/')
+        else:
+            form=LoginForm()
+        return render(request,'blogapp/login.html',{'form':form}) 
+        return HttpResponseRedirect('/dashboard/')
+
+def add_Post(request):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data['title']
+                desc = form.cleaned_data['desc']
+                pst = Post(title=title,desc=desc)
+                pst.save()
+                form = PostForm() 
+        else:
+            form = PostForm()
+        return render(request,'blogapp/addpost.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login')
+
+def update_Post(request,id):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            pi = Post.objects.get(pk=id)
+            form = PostForm(request.POST,instance=pi)
+            if form.is_valid():
+                form.save()
+        else:
+            pi = Post.objects.get(pk=id)
+            form = PostForm(instance=pi)
+        return render(request,'blogapp/update.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login')
+
+def delet_Post(request,id):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            pi = Post.objects.get(pk=id)
+            pi.delete()
+            return HttpResponseRedirect('/dashboard/')
+    else:
+        return HttpResponseRedirect('/login')
---
+++ miniblog/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path
+from blogapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('login/',views.user_login,name='login'),
+    path('signup/',views.user_signup,name='signup'),
+    path('logout/',views.user_logout,name='logout'),
+    path('addpost/',views.add_Post,name='addpost'),
+    path('updatepost/<int:id>',views.update_Post,name='updatepost'),
+    path('deletepost/<int:id>',views.delet_Post,name='deletepost'),
+    
+]
